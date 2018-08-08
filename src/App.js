345bf773612988888ff1c0d64e7e1b04e212c34b
Ivan Krasilnikov <bob.cody.in@gmail.com>
2018-08-09 00:16:44 +0300

    Add new styles

--- src/App.js
+++ src/App.js
@@ -1,40 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 import Unsplash, { toJson } from 'unsplash-js';
 
 class App extends Component {
-  constructor() {
-    super();
 
-    this.unsplash = new Unsplash({
-      applicationId: "4a098422636131849a16025acc29c09de726d2d107f4d665a67b1bc617fd6b91",
-      secret: "cc7b90fc8f2bfc4664b7cc3a72ad02d6238f6d205ddf16f5fb2cfb32e7b9b462",
-    });
+  unsplash = new Unsplash({
+    applicationId: "4a098422636131849a16025acc29c09de726d2d107f4d665a67b1bc617fd6b91",
+    secret: "cc7b90fc8f2bfc4664b7cc3a72ad02d6238f6d205ddf16f5fb2cfb32e7b9b462",
+  });
+
+  state = {
+    photoUrl: '',
+  };
+
+  // ;;life ----------------------------------------------------------------------------------------
+
+  componentDidMount() {
+    const photoUrlFromStorage = localStorage.getItem('photoUrl');
+
+    if (photoUrlFromStorage) this.setState({ photoUrl: photoUrlFromStorage });
   }
 
+  // ;;inner ---------------------------------------------------------------------------------------
+
   requestToUnsplash = () => {
-    this.unsplash.photos.listPhotos(2, 15, "latest")
-    .then(toJson)
-    .then(json => {
-      console.log(json);
-    });
+    this.unsplash.photos.getRandomPhoto()
+      .then(toJson)
+      .then(json => {
+        const requestPhotoUrl = json.urls.regular;
+
+        localStorage.setItem('photoUrl', requestPhotoUrl);
+        this.setState({ photoUrl: requestPhotoUrl });
+      });
   };
+
+  // ;;render --------------------------------------------------------------------------------------
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!!!</h1>
+      <div className="app">
+        <header className="app__header">
+          <button
+            className="app__button"
+            name="button"
+            onClick={this.requestToUnsplash}
+            type="button">
+            <img
+              className="app__logo"
+              src={`${process.env.PUBLIC_URL}/logo.svg`}
+              alt="" />
+          </button>
         </header>
-        <button
-          onClick={this.requestToUnsplash}
-          type="button">button</button>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="app__content">
+          <img
+            className="app__photo"
+            src={this.state.photoUrl}
+            alt="" />
+        </div>
       </div>
     );
   }
